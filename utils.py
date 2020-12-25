59d82f5c2cf8588661ef0c3b588a7f7839ea44ff
ianZzzzzz <zhengyh186@gmail.com>
2020-12-25 23:18:32 +0800

    time_convert和to_dict待测试

--- utils.py
+++ utils.py
@@ -12,6 +12,7 @@ def load(
     encoding='utf-8',
     columns=None
     )-> ndarray or DataFrame:
+    '''读取csv文件 返回numpy数组'''
     #if read_mode == 'cudf':import cudf as pd
     if read_mode == 'pandas' :import pandas as pd
     
@@ -23,6 +24,7 @@ def cut_nan(
     log: ndarray or DataFrame,
     key_col: List['col_number_1','col_number_2']
     )->ndarray:
+    '''删除给定列中存在空值的日志行'''
     import numpy as np
 
     pandas_ = type(pd.DataFrame([]))
@@ -42,18 +44,89 @@ def cut_nan(
     return log
 def time_convert(
     log: ndarray or DataFrame,
-    columns:List['date','time'], 
-    merge: bool
+    merge: bool,
+    date_time:List['date time'], 
+    start_time = '2018/3/1T00:00:00'
     )->ndarray: 
+    '''将日志数据的时间进行拼接转秒 秒到坐标部分未完成'''
     import numpy as np
+    
+    pandas_ = type(pd.DataFrame([]))
+        # type(type(pd.DataFrame([]))) == <class 'type'> not a str :)
+    if type(log)== pandas_ :
+        log = log.values
+    
+    date_time_array = np.zeros(len(log),dtype=np.int32)
+    # 存储一个月的秒数 需要2^22 存储一年的秒数需要2^25
+    for row in range(len(log)):
+        #1拼接
+        if merge == True: 
+            def merge_col(log: ndarray)-> str:
+                date = log[0]
+                time = log[1]
+                date_time = log[date]+'T'+'0'+log[time] # '0' especially for mathor cup data
+                return date_time
+            date_time_a16 = merge_col(log[row,date_time]) 
+            real_time = date_time_a16
+            # 此处用一个type=a16变量暂存date_time字符串
+            # WARNING 警告 两个字符串相加会因为原先的字符串类型位数不够 导致相加失败 但是不报错
+            # date_time covered the date column
+        else:   
+            real_time = log[row,date_time]
+        
+        #2转化
+        real_time = np.datetime64(real_time) 
+        start_time = np.datetime64(start_time)
+        #3转秒
+            #(1)
+        delta = int((real_time - start_time).item().total_seconds())
+        date_time_array[row] = delta
+            #(2)
+        
 
-    for col in columns:
-     for row in range(len(log)):
-            log[row,col]=
+        #4定位
+    
+    # 合并秒数列和log 清除原来date time 列
+    new_log = np.concatenate(
+         (log[:,[2,3,4]],date_time_array)
+         ,axis = 1)
+    return new_log
 
-            log[row,col]= np.datetime64(log[row,col])
-            start = log[row,2]
-            course_id = log[row,3]
-            end = c_dict[course_id][0]
-            log[row,2] = int((start - end).item().total_seconds())
-    pass
+def to_dict(
+    log: ndarray
+)-> Dict[str,ndarray]:
+    '''将日志转化为以各关键id为索引的字典'''
+    log_dict = {}
+    area_set = set(log[:,0])
+    for area in area_set:
+        mask = log[:,0]==area
+        log_dict[area] = log[mask][:,[1,2,3]]
+        
+    return log_dict
+
+def decode(
+    log_dict: Dict[str,ndarray]
+)-> Dict[str,list]:
+    for key,ndarray in log_dict.items():
+        # list version
+        list_ = ndarray.tolist()
+        for row in len(list_):
+            date = list_[row][0]
+            time = list_[row][1]
+            date_time = date +'T' +'0' +time
+        
+        # array version
+        list_ = []
+        for row in ndarray:
+            date = row[0]
+            time = ndarray[1]
+            date_time = date +'T'+'0'+time
+            date_time = np.datetime64(date_time)
+
+
+
+
+
+
+
+
