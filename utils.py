678474d7cf47eab8ab9e9ed0d9056eac98772d18
ianZzzzzz <zhengyh186@gmail.com>
2020-12-27 18:30:20 +0800

    bug finished :)

--- utils.py
+++ utils.py
@@ -9,17 +9,18 @@ def load(
     log_path: str,
     read_mode: str,
     return_mode: str,
-    encoding='utf-8',
+    encoding_='utf-8',
     columns=None
     )-> ndarray or DataFrame:
     '''读取csv文件 返回numpy数组'''
     #if read_mode == 'cudf':import cudf as pd
-    if read_mode == 'pandas' :import pandas as pd
-    
-    log = pd.read_csv(log_path,encoding=encoding,names=columns)
-    
+    if read_mode == 'pandas' :
+        import pandas as pd
+        log = pd.read_csv(log_path,encoding=encoding_,names=columns,)
+    print('21')
     if return_mode == 'df':return log
     if return_mode == 'values':return log.values
+
 def cut_nan(
     log,
     key_col:list
@@ -44,52 +45,76 @@ def cut_nan(
     return log
 def time_convert(
     log: ndarray ,
-    merge: bool,
+    merge,
     date_time_col:List['datetime'], 
-    start_time = '2018/3/1T00:00:00'
+    start_time = '2018-03-01T00:00:00'
     )->ndarray: 
     '''将日志数据的时间进行拼接转秒 秒到坐标部分未完成'''
     import numpy as np
-    
+
     pandas_ = type(pd.DataFrame([]))
         # type(type(pd.DataFrame([]))) == <class 'type'> not a str :)
     if type(log)== pandas_ :
         log = log.values
     
-    date_time_array = np.zeros(len(log),dtype=np.int32)
+    date_time_array = np.zeros((len(log),1),dtype=np.int32)
     # 存储一个月的秒数 需要2^22 存储一年的秒数需要2^25
     for row in range(len(log)):
         #1拼接
         if merge == True: 
             def merge_col(log: ndarray)-> str:
-                date = log[:,0]
-                time = log[:,1]
-                date__time = log[date]+'T'+'0'+log[time] # '0' especially for mathor cup data
+                
+                date = log[0].replace('/','-')
+                # yyyy-m-d -> yyyy-mm-dd 以适应numpy严格的格式要求
+                if len(date)<10:
+                    if len(date) == 8:
+                        date = date[0:5]+'0'+date[5:7]+'0'+date[7]
+                    else:
+                        print('date:',date)
+                        if date[6]=='-':
+                            date = date[0:5]+'0'+date[5:]
+                        else:
+                            date = date[0:8]+'0'+date[8]
+                time = log[1]
+                if time[0]=='0':time = '0'+time
+                date__time = date+'T'+time
                 return date__time
             date_time_a16 = merge_col(log[row,date_time_col]) 
             real_time = date_time_a16
+            print('real_time:',real_time)
             # 此处用一个type=a16变量暂存date_time字符串
             # WARNING 警告 两个字符串相加会因为原先的字符串类型位数不够 导致相加失败 但是不报错
             # date_time covered the date column
         else:   
-            real_time = log[row,date_time]
+            real_time = log[row,date_time_col]
         
         #2转化
+        
         real_time = np.datetime64(real_time) 
         start_time = np.datetime64(start_time)
+        #print('时间转化完成')
         #3转秒
             #(1)
         delta = int((real_time - start_time).item().total_seconds())
         date_time_array[row] = delta
+        #print('转秒完成')
             #(2)
-        
-
         #4定位
     
+
     # 合并秒数列和log 清除原来date time 列
+    print(
+        'len date_time_array: ',len(date_time_array)
+        ,'len log:',len(log[:,[2,3,4]]))
     new_log = np.concatenate(
          (log[:,[2,3,4]],date_time_array)
          ,axis = 1)
+    # 类型转换
+    new_log[:,0] = new_log[:,0].astype('u4')
+    new_log[:,1] = ((new_log[:,1].astype('f2'))*1024).astype('u2')
+    new_log[:,2] = ((new_log[:,2].astype('f2'))*1024).astype('u2')
+    new_log[:,3] = new_log[:,3].astype('u4')
+    
     return new_log
 def to_dict(
     log: ndarray
@@ -103,19 +128,17 @@ def to_dict(
         
     return log_dict
 # test
-log_path = 'D:\\zyh\\data\\com_comp\\train\\test.csv'
+log_path = 'D:\\zyh\\data\\com_comp\\train\\test2.csv'
 log_np = load(
     log_path =log_path,
     read_mode ='pandas',
     return_mode = 'values',
-    encoding = 'utf-8',
+    encoding_ = 'utf-8',
     columns = ['date','time','area','upload_quantity_GB','download_quantity_gb' ]
     )
-log_np = cut_nan(log_np,key_col = [0,1])
-log_np = time_convert(log_np,merge = True,date_time_col = [0,1])
+#log_np = cut_nan(log_np[1:5,:],key_col = [0,1])
+log_np = time_convert(log_np[1:,:],merge = True,date_time_col = [0,1])
 log_dict = to_dict(log_np)
-
-
 
 
 
