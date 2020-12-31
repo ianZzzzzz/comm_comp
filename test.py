af3b00f8dcf0de65a4a1c3f504a4099dcd556c0e
ianZzzzzz <zhengyh186@gmail.com>
2020-12-31 12:34:29 +0800

    内存不够跑不起来to_dict2 分批处理

--- test.py
+++ test.py
@@ -42,17 +42,25 @@ def _t(function):
 @_t
 def load(
     log_path: str,
+    
     read_mode: str,
     return_mode: str,
     encoding_='utf-8',
+    data_type=None,
     columns=None
     )-> ndarray or DataFrame:
     '''读取csv文件 返回numpy数组'''
     #if read_mode == 'cudf':import cudf as pd
     if read_mode == 'pandas' :
         import pandas as pd
-        log = pd.read_csv(log_path,encoding=encoding_,names=columns,)
         print('load running!')
+        log = pd.read_csv(
+            log_path
+            ,encoding=encoding_
+          #  ,names=columns
+          #  ,dtype=data_type
+            )
+        print(log.head(2))
     if return_mode == 'df':return log
     if return_mode == 'values':return log.values
 @_t
@@ -103,25 +111,32 @@ def time_convert(
         if merge == True: 
             def merge_col(log: ndarray)-> str:
                 
-                date = log[0].replace('/','-')
+                
+                def date_(date:str)-> str:
+                    date = log[0].replace('/','-')
+                    if len(date)<10:
+                       
+                        if date[0]!='2':date = '2'+date
+                        if len(date)<10:
+                            if len(date) == 8:
+                                date = date[0:5]+'0'+date[5:7]+'0'+date[7]
+                            else:
+                            
+                                if date[6]=='-':
+                                    date = date[0:5]+'0'+date[5:]
+                                else:
+                                    date = date[0:8]+'0'+date[8]
+                    return date
+                
+                date = date_(log[0])
                 # yyyy-m-d -> yyyy-mm-dd 以适应numpy严格的格式要求
                 # 原始数据88000000之后2018被记录为018
                 # 此处针对此情况处理
-                if (row>=88000000) & (date[0]=='0') :
-                    date = '2'+date
-                    print('2+ run')
-                if len(date)<10:
-                    if len(date) == 8:
-                        date = date[0:5]+'0'+date[5:7]+'0'+date[7]
-                    else:
-                       
-                        if date[6]=='-':
-                            date = date[0:5]+'0'+date[5:]
-                        else:
-                            date = date[0:8]+'0'+date[8]
+                
                 time = log[1]
                 
                 if time[1]==':':time = '0'+time
+                if time[-2]==':':time = time[:6]+'0'+time[-1]
                 date__time = date+'T'+time
                 return date__time
             date_time_a16 = merge_col(log[row,date_time_col]) 
@@ -130,8 +145,8 @@ def time_convert(
             # 此处用一个type=a16变量暂存date_time字符串
             # WARNING 警告 两个字符串相加会因为原先的字符串类型位数不够 导致相加失败 但是不报错
             # date_time covered the date column
-        else:   
-            real_time = log[row,date_time_col]
+        '''else:   
+            real_time = log[row,date_time_col]'''
         
         #2转化
         try:
@@ -261,23 +276,44 @@ def to_json(path,dict_log: Dict[int,ndarray]):
         # json不支持ndarray
         # 用json导出 array 要先 .tolist() 读取的时候直接np.array()
     '''
-    '''for k,v in dict_log.items():
+    for k,v in dict_log.items():
         dict_log[k] = v.tolist()
-'''
+
     import json
     json.dump(dict_log,open(path,'w'))
 
-log_path = 'D:\\zyh\\data\\com_comp\\train\\train_full.csv'
+log_path = 'D:\\zyh\\data\\com_comp\\train\\train_part.csv'
+
 log_np = load(
     log_path =log_path,
     read_mode ='pandas',
     return_mode = 'values',
     encoding_ = 'gbk',
-    columns = ['date','time','area','upload_quantity_GB','download_quantity_gb' ])
+    data_type =  {
+    '''name for full.csv'''
+        '日期': np.str_, '时间': np.str_,
+        '小区编号':np.uint32,
+        '上行业务量GB':np.uint32,
+        '下行业务量GB':np.uint32
+        })
+''' 中英文列名根据文件选用
+    columns = ['日期','时间''小区编号','上行业务量GB'
+        ,'下行业务量GB']
+    columns = ['date','time','area','upload_quantity_GB'
+        ,'download_quantity_gb' ]   ''' 
 log_np_convert = time_convert(log_np[1:,:],merge = True,date_time_col = [0,1])
-log_dict = to_dict_2(log_np_convert[:,:])
-ts_dict = time_map(log_dict) 
-to_json(path = 'processed_log.json',dict_log=ts_dict)
+
+log_dict_1 = to_dict_2(log_np_convert[:50000000,:])
+log_dict_2 = to_dict_2(log_np_convert[50000000:100000000,:])
+log_dict_3 = to_dict_2(log_np_convert[100000000:-1,:])
+
+ts_dict_1 = time_map(log_dict_1)
+ts_dict_2 = time_map(log_dict_2) 
+ts_dict_3 = time_map(log_dict_3)  
+to_json(path = 'processed_log_1.json',dict_log=ts_dict_1)
+to_json(path = 'processed_log_2.json',dict_log=ts_dict_2)
+to_json(path = 'processed_log_3.json',dict_log=ts_dict_3)
+
 #dataset format
 '''ds_log = np.zeros(
     (len(ts_dict),2),dtype = np.int32)'''
@@ -289,8 +325,10 @@ date = '018-04-01'
 if (row>88000000) and (date[0]=='0') :
     date = '2'+date
     print(date)
-test_log = log_np[88000000:,:]
+test_log = log_np[88000000:89000000,:]
 log_np_convert_t = time_convert(test_log,merge = True,date_time_col = [0,1])
 log_dict_t = to_dict_2(log_np_convert_t[:,:])
 ts_dict_t = time_map(log_dict_t) 
 to_json(path = 'processed_log.json',dict_log=ts_dict_t)
+
+
